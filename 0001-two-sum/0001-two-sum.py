4ccb69234bfdc767749a5a62fedfbf187e51cd7a
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-09-30 22:05:49 +0900

    Time: 61 ms (81.23%), Space: 17.7 MB (28.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,9 @@
-import bisect
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Build Hash Table (num : index)
         numMap = {}
         for i in range(len(nums)):
-            numMap[nums[i]] = i
+            numMap[nums[i]] = i    # index is overwritten if there are duplicated numbers.
 
         for i in range(len(nums)):
             complement = target - nums[i]
