850db91fc8ea73d326cb8ca5b5ae49bbf731403a
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2023-09-30 21:56:45 +0900

    Time: 66 ms (61.61%), Space: 17.4 MB (55.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,14 +2,11 @@ import bisect
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_nums = sorted(nums)
-        for i in range(len(sorted_nums)):
-            num = sorted_nums[i]
-            if target == 2 * num:
-                if num == sorted_nums[i+1]:
-                    idx = nums.index(num)
-                    return [idx, nums.index(num, idx+1)]
-                else:
-                    continue
-            if target-num in sorted_nums:
-                return [nums.index(num), nums.index(target-num)]
+        numMap = {}
+        for i in range(len(nums)):
+            numMap[nums[i]] = i
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
