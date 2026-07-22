2dceff469101f65dd7fcd91ac7c548e6f5d645f6
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-07-22 15:30:53 +0900

    Time: 471 ms (83.19%), Space: 19.2 MB (84.5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -11,5 +11,7 @@ class Solution:
         for num in nums:
             for t in range(target, num - 1, -1):
                 dp[t] = dp[t] or dp[t - num]
+            if dp[target]:
+                return True
 
         return dp[target]
