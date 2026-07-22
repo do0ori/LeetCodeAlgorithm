2f55ac0f7bf5b1c2d96521547252152050d6f7ec
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-07-22 11:25:22 +0900

    Time: 511 ms (85.12%), Space: 22.4 MB (31.53%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,30 +2,35 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         result = []
-        N = len(nums)
-        for i in range(N):
-            # skip dupicate number
+        n = len(nums)
+
+        for i in range(n - 2):
+            # prune
+            if nums[i] > 0:
+                break
+
+            # skip duplicated number
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            total = -nums[i]
-            j = i + 1
-            k = N - 1
+
+            j, k = i + 1, n - 1
+
             while j < k:
-                if nums[j] + nums[k] == total:
+                s = nums[i] + nums[j] + nums[k]
+
+                if s == 0:
                     result.append([nums[i], nums[j], nums[k]])
                     j += 1
                     k -= 1
-                    while j <= k and nums[j] == nums[j - 1]:
+
+                    while j < k and nums[j] == nums[j - 1]:
                         j += 1
-                    while j <= k and nums[k] == nums[k + 1]:
+                    while j < k and nums[k] == nums[k + 1]:
                         k -= 1
-                elif nums[j] + nums[k] < total:
+
+                elif s < 0:
                     j += 1
-                    while j <= k and nums[j] == nums[j - 1]:
-                        j += 1
                 else:
                     k -= 1
-                    while j <= k and nums[k] == nums[k + 1]:
-                        k -= 1
+
         return result
