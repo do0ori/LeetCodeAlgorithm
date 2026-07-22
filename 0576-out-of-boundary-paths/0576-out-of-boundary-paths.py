3a11eb44b69e016d04c6d2630613efe81abd2e31
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-07-22 16:52:35 +0900

    Time: 33 ms (94.49%), Space: 28.7 MB (12.65%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -7,17 +7,27 @@ dfs(r+1, c, movesLeft-1)
 + dfs(r, c-1, movesLeft-1)
 
 memoization
-= dp[r][c][k]: (r, c)에서 k번 이내로 밖으로 나가는 경로 수
+= dp[r][c][k]: (r, c)에서 movesLeft번 남았을 때 밖으로 나가는 경로 수
 """
 from functools import lru_cache
 
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        MOD = 10**9 + 7
+
         @lru_cache(None)
         def dfs(r, c, movesLeft):
-            if r > m - 1 or r < 0 or c > n - 1 or c < 0: return 1
-            if movesLeft == 0: return 0
-            return (dfs(r + 1, c, movesLeft - 1) + dfs(r - 1, c, movesLeft - 1) + dfs(r, c + 1, movesLeft - 1) + dfs(r, c - 1, movesLeft - 1)) % (10 ** 9 + 7)
+            if not (0 <= r < m and 0 <= c < n):
+                return 1
+            if movesLeft == 0:
+                return 0
+
+            return (
+                dfs(r + 1, c, movesLeft - 1)
+                + dfs(r - 1, c, movesLeft - 1)
+                + dfs(r, c + 1, movesLeft - 1)
+                + dfs(r, c - 1, movesLeft - 1)
+            ) % MOD
 
         return dfs(startRow, startColumn, maxMove)
         
