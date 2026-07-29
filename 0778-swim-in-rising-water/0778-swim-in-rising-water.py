f3436823e96c79e357fcce3e5558b5ed3d889824
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-07-29 17:42:41 +0900

    Time: 35 ms (33.27%), Space: 19.7 MB (80.8%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -2,6 +2,57 @@ import heapq
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
+        """
+        binary: O(log(N²)) = O(log N)
+        
+        V = E = N²
+        dfs/bfs: O(V + E) = O(N²)
+
+        binary * dfs/bfs = O(N² log N)
+        """
+        D = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        N = len(grid)
+        left, right = 0, N ** 2 - 1
+
+        def can_reach(t):
+            if grid[0][0] > t: return False
+
+            visited = [[False] * N for _ in range(N)]
+            visited[0][0] = True
+            stack = [(0, 0)]
+
+            while stack:
+                r, c = stack.pop()
+
+                if r == N - 1 and c == N - 1:
+                    return True
+
+                for (dr, dc) in D:
+                    nr, nc = r + dr, c + dc
+                    if not (0 <= nr < N and 0 <= nc < N): continue
+
+                    if not visited[nr][nc] and grid[nr][nc] <= t:
+                        visited[nr][nc] = True
+                        stack.append((nr, nc))
+            
+            return False
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if can_reach(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
+    
+    def swimInWater_dijkstra(self, grid: List[List[int]]) -> int:
+        """
+        V = N²
+        E = 4N²
+        O(E log V) = O(N² log 4N²) = O(N² log N)
+        """
         D = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         N = len(grid)
         
